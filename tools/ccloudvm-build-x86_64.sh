25534a36a45ae19e8b301912fb09c3d9e5ba91b1
Rob Bradford <robert.bradford@intel.com>
2018-05-01 19:39:44 +0100

    tools: Bump base OS for building with ccloudvm to bionic
    xenial is too old for vhost-vsock.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tools/ccloudvm-build-x86_64.sh
+++ tools/ccloudvm-build-x86_64.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ccloudvm create --disk 10 --cpus 2 --mem 2048 --mount nemu,none,$(realpath $PWD/..) --name nemu-x86-64 xenial
+# need bionic for vhost-vsock
+ccloudvm create --disk 10 --cpus 2 --mem 2048 --mount nemu,none,$(realpath $PWD/..) --name nemu-x86-64 bionic
 ccloudvm start nemu-x86-64
 ccloudvm run nemu-x86-64 $PWD/tools/setup-build-env.sh
 ccloudvm run nemu-x86-64 SRCDIR=$PWD $PWD/tools/build_x86_64.sh
