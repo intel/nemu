d10073197a1823df879ed7e78c0a6efee905879f
Rob Bradford <robert.bradford@intel.com>
2018-05-09 16:25:04 +0100

    configure: Remove bluez support
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tools/build_aarch64.sh
+++ tools/build_aarch64.sh
@@ -6,7 +6,6 @@ pushd $HOME/build-aarch64
 make distclean || true
 $SRCDIR/configure \
  --cross-prefix=aarch64-linux-gnu- \
- --disable-bluez \
  --disable-brlapi \
  --disable-bzip2 \
  --disable-capstone \
