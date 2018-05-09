e3cd430a4e7f5e7c301a4253f928637d6b6cfcb9
Rob Bradford <robert.bradford@intel.com>
2018-05-09 16:30:32 +0100

    tools: Alphabetically sort flags to configure
    Alphabetically sort flags for ease of comparison.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tools/build_x86_64.sh
+++ tools/build_x86_64.sh
@@ -7,73 +7,65 @@ make distclean || true
 $SRCDIR/configure \
  --disable-bluez \
  --disable-brlapi \
+ --disable-bzip2 \
+ --disable-capstone \
+ --disable-cocoa \
  --disable-curl \
  --disable-curses \
+ --disable-debug-tcg \
+ --disable-docs \
  --disable-fdt \
- --disable-netmap \
- --disable-sdl \
- --disable-virtfs \
- --disable-vde \
- --disable-vnc \
- --disable-vnc-sasl \
- --disable-vnc-jpeg \
- --disable-vnc-png \
- --disable-xen \
- --disable-linux-aio \
- --disable-tcg \
- --disable-cocoa \
- --disable-spice \
- --disable-smartcard \
- --disable-libusb \
+ --disable-gcrypt \
  --disable-glusterfs \
+ --disable-gnutls \
  --disable-gtk \
- --disable-nettle \
- --disable-vxhs \
- --disable-libxml2 \
- --disable-user \
  --disable-guest-agent \
  --disable-guest-agent-msi \
- --disable-modules \
- --disable-linux-user \
- --disable-docs \
- --disable-opengl \
- --disable-vte \
  --disable-hax \
- --disable-brlapi \
- --disable-libusb \
- --disable-usb-redir \
- --disable-lzo \
- --disable-snappy \
- --disable-curl \
- --disable-glusterfs \
  --disable-libiscsi \
  --disable-libnfs \
  --disable-libssh2 \
- --disable-bzip2 \
+ --disable-libusb \
+ --disable-libxml2 \
+ --disable-linux-aio \
+ --disable-linux-user \
  --disable-lzo \
- --disable-snappy \
- --disable-seccomp \
- --disable-tpm \
- --disable-usb-redir \
- --disable-debug-tcg \
+ --disable-modules \
+ --disable-netmap \
+ --disable-nettle \
+ --disable-opengl \
  --disable-qom-cast-debug \
+ --disable-rdma \
+ --disable-sdl \
+ --disable-seccomp \
+ --disable-smartcard \
+ --disable-snappy \
+ --disable-spice \
+ --disable-tcg \
  --disable-tcg-interpreter \
  --disable-tcmalloc \
  --disable-tools \
+ --disable-tpm \
+ --disable-usb-redir \
+ --disable-user \
+ --disable-vde \
+ --disable-virtfs \
+ --disable-vnc \
+ --disable-vnc-jpeg \
+ --disable-vnc-png \
+ --disable-vnc-sasl \
+ --disable-vte \
+ --disable-vxhs \
  --disable-xen \
- --disable-capstone \
- --disable-rdma \
- --disable-gcrypt \
- --disable-gnutls \
- --enable-kvm \
- --enable-vhost-net \
- --enable-vhost-crypto \
- --enable-vhost-scsi \
- --enable-vhost-vsock \
- --enable-vhost-user \
- --enable-rbd \
  --enable-attr \
  --enable-cap-ng \
+ --enable-kvm \
+ --enable-rbd \
+ --enable-vhost-crypto \
+ --enable-vhost-net \
+ --enable-vhost-scsi \
+ --enable-vhost-user \
+ --enable-vhost-vsock \
  --target-list=x86_64-softmmu \
  --extra-cflags=" -O3 -fno-semantic-interposition -falign-functions=32 -D_FORTIFY_SOURCE=2 -fPIE" \
  --extra-ldflags=" -pie -z noexecstack -z relro -z now" \
