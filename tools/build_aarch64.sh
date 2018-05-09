b28bbbcbe51681d04c8cd0d5a4e620e3ec3d5ca5
Rob Bradford <robert.bradford@intel.com>
2018-05-09 16:36:28 +0100

    configure: remove cocoa support
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tools/build_aarch64.sh
+++ tools/build_aarch64.sh
@@ -6,8 +6,6 @@ pushd $HOME/build-aarch64
 make distclean || true
 $SRCDIR/configure \
  --cross-prefix=aarch64-linux-gnu- \
- --disable-capstone \
- --disable-cocoa \
  --disable-curl \
  --disable-curses \
  --disable-debug-tcg \
