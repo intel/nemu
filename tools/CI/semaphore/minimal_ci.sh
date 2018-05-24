8d84b2cf05a50988efa26453fbc82ef06bce313b
Samuel Ortiz <sameo@linux.intel.com>
2018-05-24 16:08:04 +0200

    CI: Add minimal CI script for semaphore
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

---
+++ tools/CI/semaphore/minimal_ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+ARCH=x86_64
+SRCDIR=~/nemu
+
+sudo apt-get install -y mtools dosfstools
+
+sudo -E $SRCDIR/tools/CI/minimal_ci.sh -hypervisor $HOME/build-$ARCH/$ARCH-softmmu/qemu-system-$ARCH
