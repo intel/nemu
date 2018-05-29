a6e3ca0200d03dd8b05592fa82c4026657678c07
Rob Bradford <robert.bradford@intel.com>
2018-05-29 18:59:58 +0100

    tools: Add workaround for ccloudvm stop not blocking
    "ccloudvm stop" doesn't block so wait a reasonable amount of time before
    creating the second VM otherwise we'll run out of memory.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tools/build-local.sh
+++ tools/build-local.sh
@@ -5,4 +5,8 @@ export PATH=$PATH:$(go env GOPATH)/bin
 go get github.com/intel/ccloudvm/...
 ccloudvm setup
 tools/ccloudvm-build-aarch64.sh
+
+# Workaround for ccloudvm stop not waiting until VM completely stopped
+sleep 20
+
 tools/ccloudvm-build-x86_64.sh
