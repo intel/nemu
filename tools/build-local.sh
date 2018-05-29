2200aa3f5b177d45b25996502a1ce9ae42559dd9
Rob Bradford <robert.bradford@intel.com>
2018-05-29 18:40:44 +0100

    tools: Stop ccloudvm in script that starts them
    Stop the VM in the script that starts them rather than separately. This
    will also conserve host resources.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tools/build-local.sh
+++ tools/build-local.sh
@@ -6,5 +6,3 @@ go get github.com/intel/ccloudvm/...
 ccloudvm setup
 tools/ccloudvm-build-aarch64.sh
 tools/ccloudvm-build-x86_64.sh
-ccloudvm stop nemu-aarch64
-ccloudvm stop nemu-x86-64
--- tools/ccloudvm-build-x86_64.sh
+++ tools/ccloudvm-build-x86_64.sh
@@ -5,3 +5,4 @@ set -e
 ccloudvm create --disk 30 --cpus 2 --mem 2048 --mount nemu,none,$(realpath $PWD) --name nemu-x86-64 bionic || ccloudvm start nemu-x86-64
 ccloudvm run nemu-x86-64 $PWD/tools/setup-build-env.sh
 ccloudvm run nemu-x86-64 SRCDIR=$PWD $PWD/tools/build_x86_64.sh
+ccloudvm stop nemu-x86-64
