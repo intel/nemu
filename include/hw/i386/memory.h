6179e7e23930d82c8de5839138c9dd40665e201e
Samuel Ortiz <sameo@linux.intel.com>
2018-06-18 16:40:46 +0200

    i386: pc: Move memory map code in its own file
    We will also move most generic memory setting code to memory.c.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

---
+++ include/hw/i386/memory.h
@@ -0,0 +1,52 @@
+/*
+ *
+ * Copyright (c) 2018 Intel Corportation
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QEMU_I386_MEMORY_H
+#define QEMU_I386_MEMORY_H
+
+/* Memory map */
+
+#define E820_NR_ENTRIES		16
+
+struct e820_entry {
+    uint64_t address;
+    uint64_t length;
+    uint32_t type;
+} QEMU_PACKED __attribute((__aligned__(4)));
+
+struct e820_table {
+    uint32_t count;
+    struct e820_entry entry[E820_NR_ENTRIES];
+} QEMU_PACKED __attribute((__aligned__(4)));
+
+
+/* E820 types */
+#define E820_RAM        1
+#define E820_RESERVED   2
+#define E820_ACPI       3
+#define E820_NVS        4
+#define E820_UNUSABLE   5
+
+int e820_add_entry(uint64_t, uint64_t, uint32_t);
+int e820_get_num_entries(void);
+bool e820_get_entry(int, uint32_t, uint64_t *, uint64_t *);
+
+extern struct e820_table e820_reserve;
+extern struct e820_entry *e820_table;
+extern unsigned e820_entries;
+
+#endif
