0cbf48ddf67de39e750569fdc70df1112759ab28
Rob Bradford <robert.bradford@intel.com>
2018-06-19 14:55:05 +0100

    hw/i386: Allow Q35 machine type to use "sysbus-debugcon"
    It is necesary to add sysbus-debugcon to the whitelist of devices that
    the machine can use.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -305,6 +305,7 @@ static void pc_q35_machine_options(MachineClass *m)
     m->no_floppy = 1;
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_AMD_IOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_INTEL_IOMMU_DEVICE);
+    machine_class_allow_dynamic_sysbus_dev(m, "sysbus-debugcon");
     m->max_cpus = 288;
 }
 
