b56c1e6e65a6d4fd890e9fa3f0c6417b2cb21f9a
Samuel Ortiz <sameo@linux.intel.com>
2018-06-20 21:12:46 +0200

    hw: i386: Factorize CPU routines
    A few routines are now shared between pc_* and virt, including the CPU
    init one.
    We factorize those routines into an i386 specific file that is now used
    by all x86 machines.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- hw/i386/virt/virt.c
+++ hw/i386/virt/virt.c
@@ -25,6 +25,7 @@
 #include "hw/kvm/clock.h"
 
 #include "hw/i386/virt.h"
+#include "hw/i386/cpu-internal.h"
 #include "hw/i386/topology.h"
 #include "hw/i386/amd_iommu.h"
 #include "hw/i386/intel_iommu.h"
@@ -59,60 +60,6 @@
 #define DEFINE_VIRT_MACHINE(major, minor) \
     DEFINE_VIRT_MACHINE_LATEST(major, minor, false)
 
-static CpuInstanceProperties
-virt_cpu_index_to_props(MachineState *ms, unsigned cpu_index)
-{
-    MachineClass *mc = MACHINE_GET_CLASS(ms);
-    const CPUArchIdList *possible_cpus = mc->possible_cpu_arch_ids(ms);
-
-    assert(cpu_index < possible_cpus->len);
-    return possible_cpus->cpus[cpu_index].props;
-}
-
-static int64_t virt_get_default_cpu_node_id(const MachineState *ms, int idx)
-{
-   X86CPUTopoInfo topo;
-
-   assert(idx < ms->possible_cpus->len);
-   x86_topo_ids_from_apicid(ms->possible_cpus->cpus[idx].arch_id,
-                            smp_cores, smp_threads, &topo);
-   return topo.pkg_id % nb_numa_nodes;
-}
-
-static const CPUArchIdList *virt_possible_cpu_arch_ids(MachineState *ms)
-{
-    int i;
-
-    if (ms->possible_cpus) {
-        /*
-         * make sure that max_cpus hasn't changed since the first use, i.e.
-         * -smp hasn't been parsed after it
-        */
-        assert(ms->possible_cpus->len == max_cpus);
-        return ms->possible_cpus;
-    }
-
-    ms->possible_cpus = g_malloc0(sizeof(CPUArchIdList) +
-                                  sizeof(CPUArchId) * max_cpus);
-    ms->possible_cpus->len = max_cpus;
-    for (i = 0; i < ms->possible_cpus->len; i++) {
-        X86CPUTopoInfo topo;
-
-        ms->possible_cpus->cpus[i].type = ms->cpu_type;
-        ms->possible_cpus->cpus[i].vcpus_count = 1;
-        ms->possible_cpus->cpus[i].arch_id = x86_apicid_from_cpu_idx(smp_cores, smp_threads, i);
-        x86_topo_ids_from_apicid(ms->possible_cpus->cpus[i].arch_id,
-                                 smp_cores, smp_threads, &topo);
-        ms->possible_cpus->cpus[i].props.has_socket_id = true;
-        ms->possible_cpus->cpus[i].props.socket_id = topo.pkg_id;
-        ms->possible_cpus->cpus[i].props.has_core_id = true;
-        ms->possible_cpus->cpus[i].props.core_id = topo.core_id;
-        ms->possible_cpus->cpus[i].props.has_thread_id = true;
-        ms->possible_cpus->cpus[i].props.thread_id = topo.smt_id;
-    }
-    return ms->possible_cpus;
-}
-
 static void virt_machine_state_init(MachineState *machine)
 {
     //MemoryRegion *ram;
@@ -120,6 +67,8 @@ static void virt_machine_state_init(MachineState *machine)
 
     /* TODO Add the ram pointer to the QOM */
     virt_memory_init(vms);
+
+    vms->apic_id_limit = cpus_init(machine, false);
 
     kvmclock_create();
 }
@@ -184,9 +133,9 @@ static void virt_machine_class_init(MachineClass *mc)
     mc->has_hotpluggable_cpus = true;
 
     /* Machine class handlers */
-    mc->cpu_index_to_instance_props = virt_cpu_index_to_props;
-    mc->get_default_cpu_node_id = virt_get_default_cpu_node_id;
-    mc->possible_cpu_arch_ids = virt_possible_cpu_arch_ids;
+    mc->cpu_index_to_instance_props = cpu_index_to_props;
+    mc->get_default_cpu_node_id = cpu_get_default_cpu_node_id;
+    mc->possible_cpu_arch_ids = cpu_possible_cpu_arch_ids;;
 }
 
 static void virt_2_12_machine_class_init(MachineClass *mc)
