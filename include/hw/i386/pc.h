bfb7f1c8ca9dd0c859f811310be9f905bf27e845
Samuel Ortiz <sameo@linux.intel.com>
2018-07-02 19:01:20 +0200

    i386: acpi: Decouple the ACPI build from the PC machine type
    Factorize all needed information into a machine type agnostic structure
    in order to be able to build ACPI tables without depending on the pc or
    q35 machine type.
    This will allow us to reuse the ACPI build routines for the i386/virt
    machine type.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -8,6 +8,7 @@
 #include "hw/block/fdc.h"
 #include "net/net.h"
 #include "hw/i386/ioapic.h"
+#include "hw/i386/acpi.h"
 
 #include "qemu/range.h"
 #include "qemu/bitmap.h"
@@ -69,6 +70,9 @@ struct PCMachineState {
     /* Address space used by IOAPIC device. All IOAPIC interrupts
      * will be translated to MSI messages in the address space. */
     AddressSpace *ioapic_as;
+
+    /* ACPI configuration */
+    AcpiConfiguration *acpi_configuration;
 };
 
 #define PC_MACHINE_ACPI_DEVICE_PROP "acpi-device"
