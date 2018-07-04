b832b38325dcb26f706499ff31ccf0ec77588b52
Dr. David Alan Gilbert <dgilbert@redhat.com>
2018-07-04 18:51:42 +0100

    virtio-fs: Add vhost-user slave commands for mapping
    The daemon may request that fd's be mapped into the virtio-fs cache
    visible to the guest.
    These mappings are triggered by commands sent over the slave fd
    from the daemon.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- include/hw/virtio/vhost-user-fs.h
+++ include/hw/virtio/vhost-user-fs.h
@@ -23,6 +23,24 @@
 #define VHOST_USER_FS(obj) \
         OBJECT_CHECK(VHostUserFS, (obj), TYPE_VHOST_USER_FS)
 
+/* Structures carried over the slave channel back to QEMU */
+#define VHOST_USER_FS_SLAVE_ENTRIES 8
+
+/* For the flags field of VhostUserFSSlaveMsg */
+#define VHOST_USER_FS_FLAG_MAP_R (1ull << 0)
+#define VHOST_USER_FS_FLAG_MAP_W (1ull << 1)
+
+typedef struct {
+    /* Offsets within the file being mapped */
+    uint64_t fd_offset[VHOST_USER_FS_SLAVE_ENTRIES];
+    /* Offsets within the cache */
+    uint64_t c_offset[VHOST_USER_FS_SLAVE_ENTRIES];
+    /* Lengths of sections */
+    uint64_t len[VHOST_USER_FS_SLAVE_ENTRIES];
+    /* Flags, from VHOST_USER_FS_FLAG_* */
+    uint64_t flags[VHOST_USER_FS_SLAVE_ENTRIES];
+} VhostUserFSSlaveMsg;
+
 typedef struct {
     CharBackend chardev;
     char *tag;
@@ -43,5 +61,11 @@ typedef struct {
     /*< public >*/
     MemoryRegion cache;
 } VHostUserFS;
+
+/* Callbacks from the vhost-user code for slave commands */
+int vhost_user_fs_slave_map(struct vhost_dev *dev, VhostUserFSSlaveMsg *sm,
+                            int fd);
+int vhost_user_fs_slave_unmap(struct vhost_dev *dev, VhostUserFSSlaveMsg *sm);
+int vhost_user_fs_slave_sync(struct vhost_dev *dev, VhostUserFSSlaveMsg *sm);
 
 #endif /* _QEMU_VHOST_USER_FS_H */
