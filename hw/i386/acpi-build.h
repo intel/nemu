19ac0ca36afb5edc2fff902681985d7be6c9303e
Haozhong Zhang <haozhong.zhang@intel.com>
2018-07-04 17:26:41 +0200

    i386: acpi: Link guest ACPI tables when there is no firmware
    Traditionally, guest firmware is responsible for patching and
    linking the ACPI tables generated by QEMU.
    However, nofw (no firmware) machine types like e.g. virt can't rely on
    a non existent firmware to do so and thus need to do that from QEMU
    directly.
    
    Signed-off-by: Haozhong Zhang <haozhong.zhang@intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- hw/i386/acpi-build.h
+++ hw/i386/acpi-build.h
@@ -3,7 +3,10 @@
 #define HW_I386_ACPI_BUILD_H
 
 #include "hw/i386/acpi.h"
+#include "hw/acpi/bios-linker-loader.h"
+#include "qapi/error.h"
 
 void acpi_setup(MachineState *machine, AcpiConfiguration *conf);
+void acpi_link(AcpiConfiguration *conf, BIOSLinker *linker, Error **errp);
 
 #endif
