2c3ed19ea93f880952c53205cb20ecd3b1929cb4
Samuel Ortiz <sameo@linux.intel.com>
2018-07-10 11:06:33 +0200

    i386: virt: Allow sysbus console debug device
    And add it to the default x86_64 config. This gives us
    a basic serial output.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- hw/i386/virt/virt.c
+++ hw/i386/virt/virt.c
@@ -254,6 +254,7 @@ static void virt_machine_class_init(MachineClass *mc)
     mc->no_floppy = 1;
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_AMD_IOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_INTEL_IOMMU_DEVICE);
+    machine_class_allow_dynamic_sysbus_dev(mc, "sysbus-debugcon");
     mc->max_cpus = 288;
     mc->has_hotpluggable_cpus = true;
 
