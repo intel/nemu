0adfaab99775d5faaac4e0983f69ee11baae285b
Rob Bradford <robert.bradford@intel.com>
2018-07-11 14:45:41 +0100

    hw: i386: Make firmware loading non-PC specific
    Minor refactoring and relocation of code to make the firmware loading
    code not dependent on the PC specific code. This allows the code to be
    used for the virt platform.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -280,10 +280,6 @@ PCIBus *find_i440fx(void);
 extern PCIDevice *piix4_dev;
 int piix4_init(PCIBus *bus, ISABus **isa_bus, int devfn);
 
-/* pc_sysfw.c */
-void pc_system_firmware_init(MemoryRegion *rom_memory,
-                             bool isapc_ram_fw);
-
 /* acpi-build.c */
 void pc_madt_cpu_entry(AcpiDeviceIf *adev, int uid,
                        const CPUArchIdList *apic_ids, GArray *entry);
