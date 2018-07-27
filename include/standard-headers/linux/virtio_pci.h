50c58d3d4468cdc4b167e9b0ce6f468624623072
Dr. David Alan Gilbert <dgilbert@redhat.com>
2018-07-27 12:38:03 +0100

    virtio: Add shared memory capability
    Define a new capability type 'VIRTIO_PCI_CAP_SHARED_MEMORY_CFG'
    and the data structure 'virtio_pci_shm_cap' to go with it.
    They allow defining shared memory regions with sizes and offsets
    of 2^32 and more.
    Multiple instances of the capability are allowed and distinguished
    by a device-specific 'id'.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- include/standard-headers/linux/virtio_pci.h
+++ include/standard-headers/linux/virtio_pci.h
@@ -113,6 +113,8 @@
 #define VIRTIO_PCI_CAP_DEVICE_CFG	4
 /* PCI configuration access */
 #define VIRTIO_PCI_CAP_PCI_CFG		5
+/* Additional shared memory capability */
+#define VIRTIO_PCI_CAP_SHARED_MEMORY_CFG 8
 
 /* This is the PCI capability header: */
 struct virtio_pci_cap {
@@ -161,6 +163,13 @@ struct virtio_pci_common_cfg {
 struct virtio_pci_cfg_cap {
 	struct virtio_pci_cap cap;
 	uint8_t pci_cfg_data[4]; /* Data for BAR access. */
+};
+
+struct virtio_pci_shm_cap {
+	struct virtio_pci_cap cap;
+	uint32_t offset_hi;             /* Most sig 32 bits of offset */
+	uint32_t length_hi;             /* Most sig 32 bits of length */
+	uint8_t  id;                    /* To distinguish shm chunks */
 };
 
 /* Macro versions of offsets for the Old Timers! */
