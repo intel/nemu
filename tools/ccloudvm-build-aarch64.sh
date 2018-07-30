dc1631930a6a7139a1bf945cf972af3d4c363882
Rob Bradford <robert.bradford@intel.com>
2018-07-30 16:36:46 +0100

    tools: Fix ccloudvm ARM build to work in Semaphore
    The version of host 9pfs in the Semaphore VM is old and causes problems when
    trying to create files directly inside a mounted path. Instead copy the files
    into the build.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- tools/ccloudvm-build-aarch64.sh
+++ tools/ccloudvm-build-aarch64.sh
@@ -2,7 +2,9 @@
 set -e
 
 # need bionic for vhost-vsock
-ccloudvm create --disk 10 --cpus 2 --mem 2048 --mount nemu,none,$(realpath $PWD) --name nemu-aarch64 bionic || ccloudvm start nemu-aarch64
-ccloudvm run nemu-aarch64 $PWD/tools/setup-cross-env.sh
-ccloudvm run nemu-aarch64 SRCDIR=$PWD $PWD/tools/build_aarch64.sh
+ccloudvm create --disk 10 --cpus 2 --mem 2048 --name nemu-aarch64 bionic || ccloudvm start nemu-aarch64
+ccloudvm run nemu-aarch64 mkdir src
+ccloudvm copy nemu-aarch64 --recurse $PWD src/nemu
+ccloudvm run nemu-aarch64 src/nemu/tools/setup-cross-env.sh
+ccloudvm run nemu-aarch64 SRCDIR='$HOME/src/nemu' src/nemu/tools/build_aarch64.sh
 ccloudvm stop nemu-aarch64
