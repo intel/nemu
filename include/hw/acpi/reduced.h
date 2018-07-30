2c27870ddc2d92f7152a28f75b1c26f8bc80ff07
Jing Liu <jing2.liu@linux.intel.com>
2018-07-30 14:14:56 +0800

    virt: add HW-reduced ACPI shutdown support
    HW-reduced ACPI uses SLEEP_CONTROL_REG to enter S5 sleep state.
    
    Signed-off-by: Jing Liu <jing2.liu@linux.intel.com>

--- include/hw/acpi/reduced.h
+++ include/hw/acpi/reduced.h
@@ -1,6 +1,10 @@
 #ifndef HW_ACPI_REDUCED_H
 #define HW_ACPI_REDUCED_H
 
+#define ACPI_REDUCED_SLEEP_LEVEL	  5
+#define ACPI_REDUCED_SLEEP_ENABLE	  (1<<5) /* SLP_EN */
+#define ACPI_REDUCED_SLEEP_CONTROL_IOPORT 0x3B0
+
 typedef struct Aml Aml;
 
 typedef enum {
