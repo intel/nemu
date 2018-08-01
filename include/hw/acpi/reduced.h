aad1250ed86a8cbe466b486e7faba0ffd76c1aff
Jing Liu <jing2.liu@linux.intel.com>
2018-08-01 17:20:12 +0800

    hw: acpi: reduced: Add reboot support
    We only need to expose it through FADT.
    
    Signed-off-by: Jing Liu <jing2.liu@linux.intel.com>

--- include/hw/acpi/reduced.h
+++ include/hw/acpi/reduced.h
@@ -25,6 +25,8 @@ typedef struct Aml Aml;
 #define ACPI_REDUCED_SLEEP_LEVEL          5
 #define ACPI_REDUCED_SLEEP_ENABLE         (1 << 5) /* SLP_EN */
 #define ACPI_REDUCED_SLEEP_CONTROL_IOPORT 0x3B0
+#define ACPI_REDUCED_RESET_IOPORT         0x3C0
+#define ACPI_REDUCED_RESET_VALUE          4
 
 typedef enum {
     GED_CPU_HOTPLUG    = 1,
