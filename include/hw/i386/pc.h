7364a8919f12c0806380076ae3817f237cf38939
Sebastien Boeuf <sebastien.boeuf@intel.com>
2018-08-23 11:57:28 -0700

    hw: acpi: Retrieve the PCI bus from AcpiPciHpState
    Instead of using the machine type specific method find_i440fx() to
    retrieve the PCI bus, this commit aims to rely on the fact that the
    PCI bus is known by the structure AcpiPciHpState.
    
    When the structure is initialized through acpi_pcihp_init() call,
    it saves the PCI bus, which means there is no need to invoke a
    special function later on.
    
    Based on the fact that find_i440fx() was only used there, this
    patch also removes the function find_i440fx() itself from the
    entire codebase.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>
    Signed-off-by: Jing Liu <jing2.liu@linux.intel.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -260,7 +260,6 @@ PCIBus *i440fx_init(const char *host_type, const char *pci_type,
                     MemoryRegion *pci_memory,
                     MemoryRegion *ram_memory);
 
-PCIBus *find_i440fx(void);
 /* piix4.c */
 extern PCIDevice *piix4_dev;
 int piix4_init(PCIBus *bus, ISABus **isa_bus, int devfn);
