4f86f4c568f0d933312fa0ff6eb7b746c84d7c8b
Samuel Ortiz <sameo@linux.intel.com>
2018-09-05 11:12:14 +0200

    tools: Do not always download NATS images
    In order to be able to call run_nats locally, from a ccloudvm instance,
    it's more efficient to avoid downloading and installing the same
    images and go runtime every time.
    A ccloudvm VM only needs to install those once.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- tools/CI/run_nats.sh
+++ tools/CI/run_nats.sh
@@ -1,38 +1,50 @@
 #!/bin/bash
-set -e
 set -x
+
+GO_VERSION="1.10.3"
+CLEAR_VERSION=24740
+CLEAR_IMAGE=clear-$CLEAR_VERSION-cloud.img.xz
+WORKLOADS_DIR="$HOME/workloads"
+OVMF="OVMF.fd"
+
+go_install() {
+    export PATH=/usr/local/go/bin:$PATH
+    go version | grep $GO_VERSION
+    if [ $? -ne 0 ]; then
+	pushd /tmp
+	wget -nv https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
+	sudo tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
+	popd
+    fi
+
+    export GOROOT=/usr/local/go
+    export GOPATH=~/go
+
+    go version
+}
 
 sudo apt-get install -y mtools dosfstools
 
-pushd /tmp
-wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
-popd
+go_install
 
-export PATH=/usr/local/go/bin:$PATH
-export GOROOT=/usr/local/go
+go get -u github.com/intel/govmm/qemu
+go get -u golang.org/x/crypto/ssh
 
-go version
+mkdir -p $WORKLOADS_DIR
+pushd $WORKLOADS_DIR
 
-export GOPATH=~/go
+if [ ! -f "$WORKLOADS_DIR"/"$CLEAR_IMAGE" ]; then
+    wget -nv https://download.clearlinux.org/releases/$CLEAR_VERSION/clear/clear-$CLEAR_VERSION-cloud.img.xz
+    unxz clear-$CLEAR_VERSION-cloud.img.xz
+fi
 
-go get github.com/intel/govmm/qemu
-go get golang.org/x/crypto/ssh
-
-mkdir -p ~/workloads
-pushd ~/workloads
-
-CLEAR_VERSION=24740
-wget https://download.clearlinux.org/releases/$CLEAR_VERSION/clear/clear-$CLEAR_VERSION-cloud.img.xz
-unxz clear-$CLEAR_VERSION-cloud.img.xz
-
+rm -rf $OVMF
 OVMF_URL=$(curl --silent https://api.github.com/repos/rbradford/edk2/releases/latest | grep -o https://.*OVMF.fd)
 wget $OVMF_URL
 popd
 
 sudo chmod a+rw /dev/kvm
 
-pushd tools/CI/nats
-# Allow total test time to run for 20 minutes
+pushd $SRCDIR/tools/CI/nats
 go test -v -timeout 20m
 popd
