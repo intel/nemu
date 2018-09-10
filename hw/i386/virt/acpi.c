c18c5387f4895a0b14ecb82a72dfd0f5df8f8c96
Samuel Ortiz <sameo@linux.intel.com>
2018-09-10 17:37:57 +0200

    hw: i386: virt: Use virt specific MADT CPU entry routine
    acpi-build.c being built out, we need to have our own.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- hw/i386/virt/acpi.c
+++ hw/i386/virt/acpi.c
@@ -226,6 +226,44 @@ static Property virt_acpi_properties[] = {
     DEFINE_PROP_END_OF_LIST(),
 };
 
+static void madt_cpu_entry(AcpiDeviceIf *adev, int uid,
+                           const CPUArchIdList *apic_ids, GArray *entry)
+{
+    uint32_t apic_id = apic_ids->cpus[uid].arch_id;
+
+    /* ACPI spec says that LAPIC entry for non present
+     * CPU may be omitted from MADT or it must be marked
+     * as disabled. However omitting non present CPU from
+     * MADT breaks hotplug on linux. So possible CPUs
+     * should be put in MADT but kept disabled.
+     */
+    if (apic_id < 255) {
+        AcpiMadtProcessorApic *apic = acpi_data_push(entry, sizeof *apic);
+
+        apic->type = ACPI_APIC_PROCESSOR;
+        apic->length = sizeof(*apic);
+        apic->processor_id = uid;
+        apic->local_apic_id = apic_id;
+        if (apic_ids->cpus[uid].cpu != NULL) {
+            apic->flags = cpu_to_le32(1);
+        } else {
+            apic->flags = cpu_to_le32(0);
+        }
+    } else {
+        AcpiMadtProcessorX2Apic *apic = acpi_data_push(entry, sizeof *apic);
+
+        apic->type = ACPI_APIC_LOCAL_X2APIC;
+        apic->length = sizeof(*apic);
+        apic->uid = cpu_to_le32(uid);
+        apic->x2apic_id = cpu_to_le32(apic_id);
+        if (apic_ids->cpus[uid].cpu != NULL) {
+            apic->flags = cpu_to_le32(1);
+        } else {
+            apic->flags = cpu_to_le32(0);
+        }
+    }
+}
+
 static void virt_acpi_class_init(ObjectClass *class, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(class);
@@ -246,7 +284,7 @@ static void virt_acpi_class_init(ObjectClass *class, void *data)
 
     adevc->ospm_status = virt_ospm_status;
     adevc->send_event = virt_send_ged;
-    adevc->madt_cpu = pc_madt_cpu_entry;
+    adevc->madt_cpu = madt_cpu_entry;
 }
 
 static const TypeInfo virt_acpi_info = {
