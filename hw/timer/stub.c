4e31535b1051b8adc22ac5d4e5d5ea86f20669e4
Yang Zhong <yang.zhong@intel.com>
2018-09-12 16:30:36 +0800

    hw : timer: compile rtc timer out
    If we disable rtc, we need to add stub function
    for the QMP rtc support.
    
    Signed-off-by: Yang Zhong <yang.zhong@intel.com>

---
+++ hw/timer/stub.c
@@ -0,0 +1,19 @@
+/*
+ * QEMU hw stub
+ *
+ * Copyright Intel Corp. 2018
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qemu/timer.h"
+#include "qapi/qapi-commands-target.h"
+
+#ifdef TARGET_I386
+void qmp_rtc_reset_reinjection(Error **errp)
+{
+}
+#endif
