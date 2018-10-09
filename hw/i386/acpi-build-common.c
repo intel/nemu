be5d3f3c067baea713037fb95278c782008ea476
Samuel Ortiz <sameo@linux.intel.com>
2018-10-09 15:00:02 +0200

    hw: i386: Remove unneeded chipset header from the ACPI build code
    It's simply not needed anymore.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- hw/i386/acpi-build-common.c
+++ hw/i386/acpi-build-common.c
@@ -50,11 +50,8 @@
 #include "sysemu/numa.h"
 
 /* Supported chipsets: */
-#include "hw/acpi/piix4.h"
 #include "hw/acpi/pcihp.h"
-#include "hw/i386/ich9.h"
 #include "hw/pci/pci_bus.h"
-#include "hw/pci-host/q35.h"
 #include "hw/i386/x86-iommu.h"
 
 #include "hw/acpi/aml-build.h"
