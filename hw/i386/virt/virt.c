dcf22e58af385cd0dcdce2f16b088e7010bc5a03
Manohar Castelino <manohar.r.castelino@intel.com>
2018-10-09 13:48:05 -0700

    Disable the IOAPIC
    Disable the IOAPIC. Virtio devices use MSI. And we no longer
    need the IOAPIC for the GED hotplug path.
    
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>

--- hw/i386/virt/virt.c
+++ hw/i386/virt/virt.c
@@ -201,7 +201,9 @@ static void virt_machine_state_init(MachineState *machine)
     /* TODO Add the ram pointer to the QOM */
     virt_memory_init(vms);
     virt_pci_init(vms);
+#ifdef VIRT_HOTPLUG
     virt_ioapic_init(vms);
+#endif
 
     vms->acpi = virt_acpi_init(vms->gsi, vms->pci_bus);
 
