a5caa89be7da8a2854e7d2e2bd8a2de37b71dced
Sebastien Boeuf <sebastien.boeuf@intel.com>
2018-10-17 13:22:58 -0700

    hw: acpi: Multiplex GED events on a single interrupt
    This patch allows for the definition of a single interrupt for the
    GED device, relying on IO memory region to communicate the type of
    device affected by the interrupt.
    
    This way, we can support up to 32 events with a unique interrupt.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- hw/acpi/ged.c
+++ hw/acpi/ged.c
@@ -17,6 +17,66 @@
 
 #include "hw/acpi/ged.h"
 
+static uint64_t ged_read(void *opaque, hwaddr addr, unsigned size)
+{
+    uint64_t val = 0;
+    GEDState *ged_st = opaque;
+
+    switch (addr) {
+    case ACPI_GED_IRQ_SEL_OFFSET:
+        /* Read the selector value and reset it */
+        qemu_mutex_lock(&ged_st->lock);
+        val = ged_st->sel;
+        ged_st->sel = ACPI_GED_IRQ_SEL_INIT;
+        qemu_mutex_unlock(&ged_st->lock);
+        break;
+    default:
+        break;
+    }
+
+    return val;
+}
+
+/* Nothing is expected to be written to the GED memory region */
+static void ged_write(void *opaque, hwaddr addr, uint64_t data,
+                      unsigned int size)
+{
+}
+
+static const MemoryRegionOps ged_ops = {
+    .read = ged_read,
+    .write = ged_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+    .valid = {
+        .min_access_size = 4,
+        .max_access_size = 4,
+    },
+};
+
+void acpi_ged_init(MemoryRegion *as, Object *owner, GEDState *ged_st,
+                   hwaddr base_addr, uint32_t ged_irq)
+{
+    qemu_mutex_init(&ged_st->lock);
+    ged_st->irq = ged_irq;
+    memory_region_init_io(&ged_st->io, owner, &ged_ops, ged_st,
+                          "acpi-ged-event", ACPI_GED_REG_LEN);
+    memory_region_add_subregion(as, base_addr, &ged_st->io);
+}
+
+void acpi_ged_event(GEDState *ged_st, qemu_irq *irq, uint32_t ged_irq_sel)
+{
+    /* Set the GED IRQ selector to the expected device type value. This
+     * way, the ACPI method will be able to trigger the right code based
+     * on a unique IRQ.
+     */
+    qemu_mutex_lock(&ged_st->lock);
+    ged_st->sel |= ged_irq_sel;
+    qemu_mutex_unlock(&ged_st->lock);
+
+    /* Trigger the event by sending an interrupt to the guest. */
+    qemu_irq_pulse(irq[ged_st->irq]);
+}
+
 static Aml *ged_event_aml(GedEvent *event)
 {
     Aml *method;
@@ -47,17 +107,20 @@ static Aml *ged_event_aml(GedEvent *event)
     return NULL;
 }
 
-void build_ged_aml(Aml *table, const char *name,
-                   GedEvent *events, uint8_t events_size)
+void build_ged_aml(Aml *table, const char *name, uint32_t ged_irq,
+                   GedEvent *events, uint32_t events_size)
 {
     Aml *crs = aml_resource_template();
-    Aml *evt;
+    Aml *evt, *field;
     Aml *zero = aml_int(0);
-    Aml *one = aml_int(1);
     Aml *dev = aml_device("%s", name);
-    Aml *has_irq = aml_local(0);
-    Aml *while_ctx;
-    uint8_t i;
+    Aml *irq_sel = aml_local(0);
+    Aml *isel = aml_name(AML_GED_IRQ_SEL);
+    uint32_t i;
+
+    /* _CRS interrupt */
+    aml_append(crs, aml_interrupt(AML_CONSUMER, AML_LEVEL, AML_ACTIVE_HIGH,
+                                  AML_EXCLUSIVE, &ged_irq, 1));
 
     /*
      * For each GED event we:
@@ -67,90 +130,69 @@ void build_ged_aml(Aml *table, const char *name,
      */
     evt = aml_method("_EVT", 1, AML_SERIALIZED);
     {
-        Aml *irq = aml_arg(0);
         Aml *ged_aml;
-        Aml *if_ctx, *else_ctx;
+        Aml *if_ctx;
 
-        /* Local0 = One */
-        aml_append(evt, aml_store(one, has_irq));
-
+        /* Local0 = ISEL */
+        aml_append(evt, aml_store(isel, irq_sel));
 
         /*
          * Here we want to call a method for each supported GED event type.
          * The resulting ASL code looks like:
          *
-         * Local0 = One
-         * While ((Local0 == One))
+         * Local0 = ISEL
+         * If ((Local0 & irq0) == irq0)
          * {
-         *    Local0 = Zero
-         *    If (Arg0 == irq0)
-         *    {
-         *        MethodEvent0()
-         *        Local0 = Zero
-         *    }
-         *    ElseIf (Arg0 == irq1)
-         *    {
-         *        MethodEvent1()
-         *        Local0 = Zero
-         *    }
-         *    ElseIf (Arg0 == irq2)
-         *    {
-         *        MethodEvent2()
-         *        Local0 = Zero
-         *    }
+         *     MethodEvent0()
+         * }
+         *
+         * If ((Local0 & irq1) == irq1)
+         * {
+         *     MethodEvent1()
+         * }
+         *
+         * If ((Local0 & irq2) == irq2)
+         * {
+         *     MethodEvent2()
          * }
          */
 
-        /* While ((Local0 == One)) */
-        while_ctx = aml_while(aml_equal(has_irq, one));
-        {
-            else_ctx = NULL;
+        for (i = 0; i < events_size; i++) {
+            ged_aml = ged_event_aml(&events[i]);
+            if (!ged_aml) {
+                continue;
+            }
+
+            /* If ((Local1 == irq))*/
+            if_ctx = aml_if(aml_equal(aml_and(irq_sel, aml_int(events[i].selector), NULL), aml_int(events[i].selector)));
+            {
+                /* AML for this specific type of event */
+                aml_append(if_ctx, ged_aml);
+            }
 
             /*
-             * Clear loop condition, we don't want to enter an infinite loop.
-             * Local0 = Zero
+             * We append the first if to the while context.
+             * Other ifs will be elseifs.
              */
-            aml_append(while_ctx, aml_store(zero, has_irq));
-            for (i = 0; i < events_size; i++) {
-                ged_aml = ged_event_aml(&events[i]);
-                if (!ged_aml) {
-                    continue;
-                }
-
-                /* _CRS interrupt */
-                aml_append(crs, aml_interrupt(AML_CONSUMER, AML_LEVEL, AML_ACTIVE_HIGH,
-                                              AML_EXCLUSIVE, &events[i].irq, 1));
-
-                /* If ((Arg0 == irq))*/
-                if_ctx = aml_if(aml_equal(irq, aml_int(events[i].irq)));
-                {
-                    /* AML for this specific type of event */
-                    aml_append(if_ctx, ged_aml);
-                }
-
-                /*
-                 * We append the first if to the while context.
-                 * Other ifs will be elseifs.
-                 */
-                if (!else_ctx) {
-                    aml_append(while_ctx, if_ctx);
-                } else {
-                    aml_append(else_ctx, if_ctx);
-                    aml_append(while_ctx, else_ctx);
-                }
-
-                if (i != events_size - 1) {
-                    else_ctx = aml_else();
-                }
-            }
+            aml_append(evt, if_ctx);
         }
-
-        aml_append(evt, while_ctx);
     }
 
     aml_append(dev, aml_name_decl("_HID", aml_string("ACPI0013")));
     aml_append(dev, aml_name_decl("_UID", zero));
     aml_append(dev, aml_name_decl("_CRS", crs));
+
+    /* Append IO region */
+    aml_append(dev, aml_operation_region(AML_GED_IRQ_REG, AML_SYSTEM_IO,
+               aml_int(ACPI_GED_EVENT_IO_BASE + ACPI_GED_IRQ_SEL_OFFSET),
+               ACPI_GED_IRQ_SEL_LEN));
+    field = aml_field(AML_GED_IRQ_REG, AML_DWORD_ACC, AML_NOLOCK,
+                      AML_WRITE_AS_ZEROS);
+    aml_append(field, aml_named_field(AML_GED_IRQ_SEL,
+                                      ACPI_GED_IRQ_SEL_LEN * 8));
+    aml_append(dev, field);
+
+    /* Append _EVT method */
     aml_append(dev, evt);
 
     aml_append(table, dev);
