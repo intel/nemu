bc890c10b82c642630c71576dac1e44bf75ff0eb
Samuel Ortiz <sameo@linux.intel.com>
2018-11-09 15:54:44 +0100

    target: arm: Move watchpoints APIs to helper.c
    Here again, those APIs are not TCG dependent and can move to the always
    built helper.c.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Robert Bradford <robert.bradford@intel.com>

--- target/arm/internals.h
+++ target/arm/internals.h
@@ -937,6 +937,12 @@ bool pmsav8_mpu_lookup(CPUARMState *env, uint32_t address,
                        int *prot, bool *is_subpage,
                        ARMMMUFaultInfo *fi, uint32_t *mregion);
 
+/*
+ * Returns true when the current CPU execution context matches
+ * the watchpoint or the breakpoint pointed at by n.
+ */
+bool bp_wp_matches(ARMCPU *cpu, int n, bool is_wp);
+
 #ifdef TARGET_AARCH64
 void aarch64_cpu_dump_state(CPUState *cs, FILE *f,
                             fprintf_function cpu_fprintf, int flags);
