36bfba7a718656fee68aec93e0772715829957d6
NEMU CI <noreply@intel.com>
2018-11-30 17:01:56 +0000

    Automatic code removal
    Before:
    ───────────────────────────────────────────────────────────────────────────────
    Language                 Files     Lines     Code  Comments   Blanks Complexity
    ───────────────────────────────────────────────────────────────────────────────
    C                         2003   1375344  1081438    124914   168992     135272
    C Header                  1447    247152   161909     56756    28487       3597
    
    After:
    ───────────────────────────────────────────────────────────────────────────────
    Language                 Files     Lines     Code  Comments   Blanks Complexity
    ───────────────────────────────────────────────────────────────────────────────
    C                          610    358549   271998     40480    46071      41721
    C Header                   603    115076    67468     33880    13728       1612

--- include/hw/mips/bios.h
+++
@@ -1,9 +0,0 @@
-#include "qemu/units.h"
-#include "cpu.h"
-
-#define BIOS_SIZE (4 * MiB)
-#ifdef TARGET_WORDS_BIGENDIAN
-#define BIOS_FILENAME "mips_bios.bin"
-#else
-#define BIOS_FILENAME "mipsel_bios.bin"
-#endif
