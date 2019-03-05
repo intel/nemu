f8294c119e6d1ab7380ad3e3fb92174e556aea2a
Stefan Hajnoczi <stefanha@redhat.com>
2019-03-05 10:15:24 +0000

    virtiofsd: forbid multiqueue until it has been audited
    The passthrough_ll.c code was written with multi-threading in mind but
    not with malicious clients.  The code needs to be audited again for
    race conditions that could be triggered by malicious clients.  Disable
    multi-queue support for now.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- contrib/virtiofsd/fuse_virtio.c
+++ contrib/virtiofsd/fuse_virtio.c
@@ -596,12 +596,16 @@ static void fv_queue_set_started(VuDev *dev, int qidx, bool started)
         fprintf(stderr, "%s: qidx=%d started=%d\n", __func__, qidx, started);
         assert(qidx>=0);
 
-        if (qidx == 0) {
-                /* This is a notification queue for us to tell the guest things
-                 *  we don't expect
-                 * any incoming from the guest here.
-                 */
-                return;
+        /*
+	 * Ignore additional request queues for now.  passthrough_ll.c must be
+         * audited for thread-safety issues first.  It was written with a
+         * well-behaved client in mind and may not protect against all types of
+         * races yet.
+         */
+        if (qidx > 1) {
+                fprintf(stderr, "%s: multiple request queues not yet implemented, please only configure 1 request queue\n",
+                        __func__);
+                exit(EXIT_FAILURE);
         }
 
         if (started) {
