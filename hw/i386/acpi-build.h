eb9dd4afc03a8ef2ed820c41e3b4cf43ad3f5d8d
Samuel Ortiz <sameo@linux.intel.com>
2019-04-10 20:49:01 +0800

    hw: acpi: Factor some i386 ACPI code out
    We move some structure into the architecture agnostic
    include/hw/acpi/acpi.h header, and we move the ACPI link
    API into hw/acpi/link.c
    
    We need to do so in preparation for the generic HW reduced ACPI
    implementation.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- hw/i386/acpi-build.h
+++ hw/i386/acpi-build.h
@@ -2,7 +2,7 @@
 #ifndef HW_I386_ACPI_BUILD_H
 #define HW_I386_ACPI_BUILD_H
 
-#include "hw/i386/acpi.h"
+#include "hw/acpi/acpi.h"
 
 void acpi_setup(MachineState *machine, AcpiConfiguration *conf);
 
