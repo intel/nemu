aa0c775c768a452c544925c603a480eda8bbedfc
Samuel Ortiz <sameo@linux.intel.com>
2019-04-10 20:56:13 +0800

    acpi: reduced: GED AML generation routine
    We generate the AML code for handling GEDs.
    The build_ged_aml routine takes a GedEvent array that maps a specific
    GED event to an IRQ number. Then we use that array to build both the
    _CRS and the _EVT section of the GED device.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/hw/acpi/reduced.h
+++ include/hw/acpi/reduced.h
@@ -1,11 +1,22 @@
 #ifndef HW_ACPI_REDUCED_H
 #define HW_ACPI_REDUCED_H
 
-typedef struct MemMapEntry {
-    hwaddr base;
-    hwaddr size;
-} MemMapEntry;
+typedef struct Aml Aml;
+
+typedef enum {
+    GED_CPU_HOTPLUG    = 1,
+    GED_MEMORY_HOTPLUG = 2,
+    GED_PCI_HOTPLUG    = 3,
+    GED_NVDIMM_HOTPLUG = 4,
+} GedEventType;
+
+typedef struct GedEvent {
+    uint32_t     irq;
+    GedEventType event;
+} GedEvent;
 
 void acpi_reduced_setup(MachineState *machine, AcpiConfiguration *conf);
+void build_ged_aml(Aml *table, const char* name,
+                   GedEvent *events, uint8_t events_size);
 
 #endif
