6f06f3483670b679cabc83bc9cedfde5a094e21d
Manohar Castelino <manohar.r.castelino@intel.com>
2019-04-22 14:38:04 +0800

    acpi: Specify ACPI PCI Hotplug IO Port address as a parameter
    Specify ACPI PCI Hotplug IO Port address as a parameter. This will
    allow each platform to choose its own memory layout.
    
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>

--- hw/acpi/aml-build.c
+++ hw/acpi/aml-build.c
@@ -2385,28 +2385,28 @@ Aml *build_pci_host_bridge(Aml *table, AcpiPciBus *pci_host)
     return scope;
 }
 
-void build_acpi_pci_hotplug(Aml *scope)
+void build_acpi_pci_hotplug(Aml *scope, uint16_t acpi_iobase_addr)
 {
     Aml *field;
     Aml *method;
 
-    aml_append(scope,
-        aml_operation_region("PCST", AML_SYSTEM_IO, aml_int(0xae00), 0x08));
+    aml_append(scope, aml_operation_region("PCST", AML_SYSTEM_IO,
+                              aml_int(acpi_iobase_addr), ACPI_PCI_PCST_LEN));
     field = aml_field("PCST", AML_DWORD_ACC, AML_NOLOCK, AML_WRITE_AS_ZEROS);
-    aml_append(field, aml_named_field("PCIU", 32));
-    aml_append(field, aml_named_field("PCID", 32));
+    aml_append(field, aml_named_field("PCIU", ACPI_PCI_UP_LEN * 8));
+    aml_append(field, aml_named_field("PCID", ACPI_PCI_DOWN_LEN * 8));
     aml_append(scope, field);
 
-    aml_append(scope,
-        aml_operation_region("SEJ", AML_SYSTEM_IO, aml_int(0xae08), 0x04));
+    aml_append(scope, aml_operation_region("SEJ", AML_SYSTEM_IO,
+                         aml_int(acpi_iobase_addr + ACPI_PCI_EJ_BASE), ACPI_PCI_EJ_LEN));
     field = aml_field("SEJ", AML_DWORD_ACC, AML_NOLOCK, AML_WRITE_AS_ZEROS);
-    aml_append(field, aml_named_field("B0EJ", 32));
+    aml_append(field, aml_named_field("B0EJ", ACPI_PCI_EJ_LEN * 8));
     aml_append(scope, field);
 
-    aml_append(scope,
-        aml_operation_region("BNMR", AML_SYSTEM_IO, aml_int(0xae10), 0x04));
+    aml_append(scope, aml_operation_region("BNMR", AML_SYSTEM_IO,
+                         aml_int(acpi_iobase_addr + ACPI_PCI_SEL_BASE), ACPI_PCI_SEL_LEN));
     field = aml_field("BNMR", AML_DWORD_ACC, AML_NOLOCK, AML_WRITE_AS_ZEROS);
-    aml_append(field, aml_named_field("BNUM", 32));
+    aml_append(field, aml_named_field("BNUM", ACPI_PCI_SEL_LEN * 4));
     aml_append(scope, field);
 
     aml_append(scope, aml_mutex("BLCK", 0));
@@ -2582,6 +2582,7 @@ void acpi_dsdt_add_pci_bus(Aml *dsdt, AcpiPciBus *pci_host)
 {
     Aml *dev, *pci_scope, *hp_scope;
     uint16_t pci_segment = pci_host->pci_segment;
+    uint16_t acpi_iobase_addr =  pci_host->acpi_iobase_addr;
 
     dev = aml_device("\\_SB.PCI%x", pci_segment);
     aml_append(dev, aml_name_decl("_HID", aml_eisaid("PNP0A08")));
@@ -2596,7 +2597,7 @@ void acpi_dsdt_add_pci_bus(Aml *dsdt, AcpiPciBus *pci_host)
 
     /* PCIHP */
     hp_scope =  aml_scope("\\_SB.PCI%x", pci_segment);
-    build_acpi_pci_hotplug(hp_scope);
+    build_acpi_pci_hotplug(hp_scope, acpi_iobase_addr);
     build_append_pci_bus_devices(hp_scope, pci_host->pci_bus, false);
     aml_append(dsdt, hp_scope);
 
