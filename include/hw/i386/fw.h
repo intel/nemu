f712c8b6ede5ebf44767b3413819c74bb181ffea
Yang Zhong <yang.zhong@intel.com>
2019-04-22 15:29:27 +0800

    hw: i386: Remove the pc header dependency from the fw  code
    It's only a matter of moving the FW IO base definition to the correct
    header file (fw.h).
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/hw/i386/fw.h
+++ include/hw/i386/fw.h
@@ -13,6 +13,8 @@
 #define FW_CFG_E820_TABLE (FW_CFG_ARCH_LOCAL + 3)
 #define FW_CFG_HPET (FW_CFG_ARCH_LOCAL + 4)
 
+#define FW_CFG_IO_BASE 0x510
+
 /* fw_cfg machine ids */
 enum {
     X86_I440FX = 1,
@@ -22,7 +24,6 @@ enum {
     X86_XENPV,
     X86_VIRT,
 };
-
 
 FWCfgState *fw_cfg_init(MachineState *ms, uint16_t boot_cpus, const CPUArchIdList *cpus, unsigned apic_id_limit);
 void pc_system_rom_init(MemoryRegion *rom_memory, bool isapc_ram_fw);
